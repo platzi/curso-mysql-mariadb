91d66469829ddb120a8559660ebb1d30b0e87220
RetaxMaster <retaxmaster@hotmail.com>
2022-08-02 13:15:58 -0500

    13 Actualizando

---
+++ 7-updating-info.sql
@@ -0,0 +1,18 @@
+USE metro_cdmx;
+
+UPDATE `stations`
+SET name = "Lázaro Cárdenas"
+WHERE id = 1;
+
+UPDATE `stations`
+SET name = "Ferrería"
+WHERE id = 2;
+
+UPDATE `stations`
+SET name = "Pantitlán" WHERE id = 3;
+
+UPDATE `stations`
+SET name = "Tacuba" WHERE id = 4;
+
+UPDATE `stations`
+SET name = "Martín Carrera" WHERE id = 5;
