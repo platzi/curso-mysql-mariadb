fc6d48462fce61f0e40a50c079f575129e31d187
RetaxMaster <retaxmaster@hotmail.com>
2022-08-02 12:42:34 -0500

    12 Llenando base de datos

---
+++ 4-insert-lines.sql
@@ -0,0 +1,16 @@
+USE metro_cdmx;
+
+-- Insertar las líneas del metro
+INSERT INTO `lines` (name, color) VALUES
+("Línea 1", "Rosa"),
+("Línea 2", "Azul"),
+("Línea 3", "Verde olivo"),
+("Línea 4", "Cian"),
+("Línea 5", "Amarillo"),
+("Línea 6", "Rojo"),
+("Línea 7", "Naranja"),
+("Línea 8", "Verder"),
+("Línea 9", "Café"),
+("Línea A", "Morado"),
+("Línea B", "Verde y gris"),
+("Línea 12", "Oro");
