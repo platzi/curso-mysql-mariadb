4296c31488a2c757e48251b48f9ac22efd055737
RetaxMaster <retaxmaster@hotmail.com>
2022-08-03 12:19:19 -0500

    20 solucion reto geo

--- 19-geo-select.sql
+++ 19-geo-select.sql
@@ -14,3 +14,22 @@ ST_Distance_Sphere(
     POINT(-99.14912224, 19.42729875),
     POINT(-99.13303971, 19.42543703)
 ) / 1000 AS distance;
+
+-- Calculamos en kilometros con consultas anidadas
+SELECT
+ST_Distance_Sphere(
+    (
+        SELECT `locations`.`location`
+        FROM `locations`
+        INNER JOIN `stations`
+        ON `stations`.`id` = `locations`.`station_id`
+        WHERE `stations`.`name` = "Balderas"
+    ),
+    (
+        SELECT `locations`.`location`
+        FROM `locations`
+        INNER JOIN `stations`
+        ON `stations`.`id` = `locations`.`station_id`
+        WHERE `stations`.`name` = "Pino Suarez"
+    )
+) / 1000 AS distance;
